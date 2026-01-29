125e398a587aad23c36bf33e5a9e6b50f2d3f9e2
Rajiv Subramanian M <rajiv.m1991@gmail.com>
2026-01-30 00:56:36 +0530

    Fix cask URL to match actual release filename

--- Casks/droiddock.rb
+++ Casks/droiddock.rb
@@ -2,7 +2,7 @@ cask "droiddock" do
   version "0.2.2"
   sha256 "bbcfaf8f014308e017fec04bb308d86a1f24c9c63964960539552d6e1879d5c3"
 
-  url "https://github.com/rajivm1991/DroidDock/releases/download/v#{version}/rw.86958.DroidDock_#{version}_aarch64.dmg"
+  url "https://github.com/rajivm1991/DroidDock/releases/download/v#{version}/rw.86958.DroidDock_0.2.2_aarch64.dmg"
   name "DroidDock"
   desc "Desktop tool for Android device management"
   homepage "https://github.com/rajivm1991/DroidDock"
