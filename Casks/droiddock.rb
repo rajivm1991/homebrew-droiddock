7feeec2348d527bf05d55e6c110e771615f12a23
Rajiv Subramanian M <rajiv.m1991@gmail.com>
2026-02-19 12:36:55 +0530

    fix: use universal.dmg for v0.4.0 cask

--- Casks/droiddock.rb
+++ Casks/droiddock.rb
@@ -1,8 +1,8 @@
 cask "droiddock" do
   version "0.4.0"
-  sha256 "f71b38532c8d40d2ed734220514fd9cdd19b9672fbd163ced5212d8417c41009"
+  sha256 "c8c262d1a879f09db1a0e07f4bdecfdeeb5ded8bf66444887680dfdfb7ef85c9"
 
-  url "https://github.com/rajivm1991/DroidDock/releases/download/v#{version}/DroidDock_#{version}_aarch64.dmg"
+  url "https://github.com/rajivm1991/DroidDock/releases/download/v#{version}/DroidDock_#{version}_universal.dmg"
   name "DroidDock"
   desc "Desktop tool for Android device management"
   homepage "https://github.com/rajivm1991/DroidDock"
