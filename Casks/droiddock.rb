52f52e23e21a7ac3db715ee5df23480daeef592c
Rajiv Subramanian M <rajiv.m1991@gmail.com>
2026-02-23 18:51:26 +0530

    chore: update droiddock to v0.4.1

--- Casks/droiddock.rb
+++ Casks/droiddock.rb
@@ -1,6 +1,6 @@
 cask "droiddock" do
-  version "0.4.0"
-  sha256 "c8c262d1a879f09db1a0e07f4bdecfdeeb5ded8bf66444887680dfdfb7ef85c9"
+  version "0.4.1"
+  sha256 "8fb47380a5020d76e654986d632ebfd874c7561b8826272e3d6dd92d701efc5d"
 
   url "https://github.com/rajivm1991/DroidDock/releases/download/v#{version}/DroidDock_#{version}_universal.dmg"
   name "DroidDock"
